83ad9e09569fd73a29961f919fe7aa4f640447ab
elfcrzgr <ferhat_coker@yahoo.com>
2026-07-19 12:38:23 +0300

    Update football.js

--- football.js
+++ football.js
@@ -482,7 +482,12 @@ async function checkAndSendNotifications(newMatches) {
                                 homeScore: currH, 
                                 awayScore: currA, 
                                 matchMinute: isFinished ? "MS" : String(liveMin) 
-                            } 
+                            },
+                            // 🚀 APPLE'IN ARKA PLANDA GÜNCELLEMEYİ REDDETMEMESİ İÇİN ŞART:
+                            "alert": {
+                                "title": "Skor Güncellendi",
+                                "body": `Dakika: ${liveMin}`
+                                }
                         } 
                     };
                     notification.topic = "com.elfcrzgr.macsaati.push-type.liveactivity"; 
