03d862665488ee09c12fe89dc7fb7a366b1fed8e
elfcrzgr <ferhat_coker@yahoo.com>
2026-07-19 14:41:24 +0300

    Update football.js

--- football.js
+++ football.js
@@ -313,7 +313,7 @@ const getFootBroadcaster = (utId, hName, aName, tName, utName) => {
 };
 
 const ELITE_FOOT_IDS = [17, 8, 35, 23, 34, 52, 37, 38, 238, 36, 19, 96, 97, 98, 7, 679, 17015, 16, 1, 133, 270, 53, 335, 13363];
-const REGULAR_FOOT_IDS = [299, 155, 325, 955, 18, 6516, 242, 11415, 11416, 11417, 15938, 851, 853];
+const REGULAR_FOOT_IDS = [299, 155, 325, 955, 18, 6516, 242, 11415, 11416, 11417, 15938, 851];
 const NATIONAL_LEAGUES = [16, 1, 133, 270, 299, 851];
 const ALL_FOOT_TARGETS = [...ELITE_FOOT_IDS, ...REGULAR_FOOT_IDS];
 
@@ -330,7 +330,7 @@ const footballLeagues = {
     6516: "Kulüp Hazırlık Maçları", 325: "Brezilya Serie A",
     155: "Arjantin Liga Profesional", 242: "MLS", 13363: "USL Championship",
     335: "Fransa Kupası", 955: "Suudi Arabistan Pro Lig", 18: "İngiltere Championship",
-    851: "Uluslararası Hazırlık Maçları", 853: "Kulüp Hazırlık Maçları"
+    851: "Uluslararası Hazırlık Maçları"
 };
 
 const nationalTeamCodes = {
