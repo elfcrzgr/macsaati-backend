de9ae158e39d406e4176bd3ec39b5186d1fd4a5b
elfcrzgr <ferhat_coker@yahoo.com>
2026-08-04 15:20:31 +0300

    Update yayinci.js

--- yayinci.js
+++ yayinci.js
@@ -1,6 +1,387 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
+// =========================================================================
+// 🔥 TAKIM ADI STANDARTLAŞTIRMA (Sofascore Formatına)
+// =========================================================================
+const teamNameMapping = {
+    // Türkiye Ligleri
+    "FENERBAHÇE": "Fenerbahçe SK",
+    "FENERBAHCE": "Fenerbahçe SK",
+    "GALATASARAY": "Galatasaray",
+    "BESIKTAS": "Beşiktaş",
+    "BEŞIKTAŞ": "Beşiktaş",
+    "TRABZONSPOR": "Trabzonspor",
+    "BASAKSEHIR": "Başakşehir",
+    "BAŞAKŞEHIR": "Başakşehir",
+    "ISTANBUL BASAKSEHIR": "Başakşehir",
+    "İSTANBUL BAŞAKŞEHIR": "Başakşehir",
+    "SIVASSPOR": "Sivasspor",
+    "KONYASPOR": "Konyaspor",
+    "ANTALYASPOR": "Antalyaspor",
+    "ALANYASPOR": "Alanyaspor",
+    "KAYSERISPOR": "Kayserispor",
+    "RIZESPOR": "Rizespor",
+    "KASIMPASA": "Kasımpaşa",
+    "KASIMPASA": "Kasımpaşa",
+    "ISTANBULSPOR": "İstanbulspor",
+    "GÖZTEPE": "Göztepe",
+    "ALTAY": "Altay",
+    "ERZURUMSPOR": "Erzurumspor",
+    "ERZURUMSPOR FK": "Erzurumspor",
+    "ADANA DEMIRSPOR": "Adana Demirspor",
+    "GAZIANTEP FK": "Gaziantep FK",
+    "GAZIŞEHIR GAZIANTEP": "Gaziantep FK",
+    
+    // Premier League
+    "REAL MADRID": "Real Madrid",
+    "BARCELONA": "Barcelona",
+    "MANCHESTER UNITED": "Manchester United",
+    "MANCHESTER CITY": "Manchester City",
+    "ARSENAL": "Arsenal",
+    "CHELSEA": "Chelsea",
+    "LIVERPOOL": "Liverpool",
+    "TOTTENHAM": "Tottenham Hotspur",
+    "TOTTENHAM HOTSPUR": "Tottenham Hotspur",
+    "NEWCASTLE UNITED": "Newcastle United",
+    "NEWCASTLE": "Newcastle United",
+    "ASTON VILLA": "Aston Villa",
+    "BRIGHTON": "Brighton",
+    "BRIGHTON HOVE ALBION": "Brighton",
+    "CRYSTAL PALACE": "Crystal Palace",
+    "EVERTON": "Everton",
+    "FULHAM": "Fulham",
+    "IPSWICH TOWN": "Ipswich Town",
+    "IPSWICH": "Ipswich Town",
+    "LEICESTER CITY": "Leicester City",
+    "LEICESTER": "Leicester City",
+    "MANCHESTER": "Manchester United",
+    "NOTTINGHAM FOREST": "Nottingham Forest",
+    "NOTTINGHAM": "Nottingham Forest",
+    "BRENTFORD": "Brentford",
+    "WEST HAM UNITED": "West Ham United",
+    "WEST HAM": "West Ham United",
+    "WOLVERHAMPTON": "Wolverhampton Wanderers",
+    "WOLVERHAMPTON WANDERERS": "Wolverhampton Wanderers",
+    "BOURNEMOUTH": "Bournemouth",
+    "SOUTHAMPTON": "Southampton",
+    "LUTON TOWN": "Luton Town",
+    "LUTON": "Luton Town",
+    "SHEFFIELD UNITED": "Sheffield United",
+    "SHEFFIELD": "Sheffield United",
+    "BURNLEY": "Burnley",
+    "LEEDS UNITED": "Leeds United",
+    "LEEDS": "Leeds United",
+
+    // La Liga
+    "ATLETICO MADRID": "Atletico Madrid",
+    "ATLÉTICO MADRID": "Atletico Madrid",
+    "REAL BETIS": "Real Betis",
+    "BETIS": "Real Betis",
+    "SEVILLA": "Sevilla",
+    "SEVILLE": "Sevilla",
+    "VALENCIA": "Valencia",
+    "VILLARREAL": "Villarreal",
+    "REAL SOCIEDAD": "Real Sociedad",
+    "SOCIEDAD": "Real Sociedad",
+    "ATHLETIC BILBAO": "Athletic Bilbao",
+    "ATHLETIC": "Athletic Bilbao",
+    "MALLORCA": "Mallorca",
+    "LEGANES": "Leganes",
+    "RAYO VALLECANO": "Rayo Vallecano",
+    "RAYO": "Rayo Vallecano",
+    "OSASUNA": "Osasuna",
+    "GETAFE": "Getafe",
+    "CELTA VIGO": "Celta Vigo",
+    "CELTA": "Celta Vigo",
+    "ALMERIA": "Almeria",
+    "ALMERÍA": "Almeria",
+
+    // Serie A
+    "JUVENTUS": "Juventus",
+    "AC MILAN": "Milan",
+    "AC-MILAN": "Milan",
+    "MILAN": "Milan",
+    "INTER": "Inter",
+    "INTER MILAN": "Inter",
+    "ROMA": "Roma",
+    "AS ROMA": "Roma",
+    "LAZIO": "Lazio",
+    "NAPOLI": "Napoli",
+    "ATALANTA": "Atalanta",
+    "FIORENTINA": "Fiorentina",
+    "TORINO": "Torino",
+    "GENOA": "Genoa",
+    "MONZA": "Monza",
+    "CAGLIARI": "Cagliari",
+    "VENEZIA": "Venezia",
+    "LECCE": "Lecce",
+    "SALERNITANA": "Salernitana",
+    "SASSUOLO": "Sassuolo",
+    "HELLAS VERONA": "Hellas Verona",
+    "VERONA": "Hellas Verona",
+    "PARMA": "Parma",
+    "BOLOGNA": "Bologna",
+
+    // Ligue 1
+    "PARIS SAINT-GERMAIN": "Paris Saint-Germain",
+    "PARIS SG": "Paris Saint-Germain",
+    "PSG": "Paris Saint-Germain",
+    "OLYMPIQUE LYON": "Olympique Lyon",
+    "LYON": "Olympique Lyon",
+    "OLYMPIQUE MARSEILLE": "Marseille",
+    "MARSEILLE": "Marseille",
+    "OM": "Marseille",
+    "AS MONACO": "Monaco",
+    "MONACO": "Monaco",
+    "LILLE": "Lille",
+    "LOSC LILLE": "Lille",
+    "NICE": "Nice",
+    "OGC NICE": "Nice",
+    "RENNES": "Rennes",
+    "STADE RENNAIS": "Rennes",
+    "STRASBOURG": "Strasbourg",
+    "RC STRASBOURG": "Strasbourg",
+    "BREST": "Brest",
+    "STADE BREST": "Brest",
+    "STADE BRIOCHIN": "Brest",
+    "ANGERS": "Angers",
+    "SCO ANGERS": "Angers",
+    "MONTPELLIER": "Montpellier",
+    "MONTPELLIER HSC": "Montpellier",
+    "NANTES": "Nantes",
+    "FC NANTES": "Nantes",
+    "TOULOUSE": "Toulouse",
+    "TFC": "Toulouse",
+    "LENS": "Lens",
+    "RC LENS": "Lens",
+
+    // Bundesliga
+    "BAYERN MÜNCHEN": "Bayern Munich",
+    "BAYERN MUNCHEN": "Bayern Munich",
+    "BAYERN": "Bayern Munich",
+    "BORUSSIA DORTMUND": "Borussia Dortmund",
+    "DORTMUND": "Borussia Dortmund",
+    "BVB": "Borussia Dortmund",
+    "BORUSSIA MÖNCHENGLADBACH": "Borussia Monchengladbach",
+    "BORUSSIA MÖNCHEN": "Borussia Monchengladbach",
+    "MÖNCHENGLADBACH": "Borussia Monchengladbach",
+    "GLADBACH": "Borussia Monchengladbach",
+    "BAYER LEVERKUSEN": "Bayer Leverkusen",
+    "LEVERKUSEN": "Bayer Leverkusen",
+    "RB LEIPZIG": "RB Leipzig",
+    "LEIPZIG": "RB Leipzig",
+    "SCHALKE 04": "FC Schalke 04",
+    "SCHALKE": "FC Schalke 04",
+    "MAINZ 05": "1. FSV Mainz 05",
+    "MAINZ": "1. FSV Mainz 05",
+    "HOFFENHEIM": "TSG Hoffenheim",
+    "AUGSBURG": "FC Augsburg",
+    "FCA": "FC Augsburg",
+    "EINTRACHT FRANKFURT": "Eintracht Frankfurt",
+    "FRANKFURT": "Eintracht Frankfurt",
+    "UNION BERLIN": "Union Berlin",
+    "BERLIN": "Union Berlin",
+    "HERTHA BERLIN": "Hertha Berlin",
+    "HERTHA": "Hertha Berlin",
+    "FREIBURG": "SC Freiburg",
+    "STUTTART": "VfB Stuttgart",
+    "STUTTGART": "VfB Stuttgart",
+    "HAMBURG": "Hamburger SV",
+    "HAMBURG SV": "Hamburger SV",
+    "COLOGNE": "Cologne",
+    "KÖLN": "Cologne",
+    "FC KÖLN": "Cologne",
+    "WOLFSBURG": "VfL Wolfsburg",
+    "WERDER BREMEN": "Werder Bremen",
+    "BREMEN": "Werder Bremen",
+
+    // Eredivisie
+    "AJAX": "Ajax",
+    "AFC AJAX": "Ajax",
+    "PSV": "PSV Eindhoven",
+    "PSV EINDHOVEN": "PSV Eindhoven",
+    "FEYENOORD": "Feyenoord",
+    "AZ ALKMAAR": "AZ Alkmaar",
+    "AZ": "AZ Alkmaar",
+    "VITESSE": "Vitesse",
+    "UTRECHT": "Utrecht",
+    "FC UTRECHT": "Utrecht",
+    "GRONINGEN": "Groningen",
+    "FC GRONINGEN": "Groningen",
+    "TWENTE": "Twente",
+    "FC TWENTE": "Twente",
+    "HEERENVEEN": "Heerenveen",
+    "SC HEERENVEEN": "Heerenveen",
+    "GO AHEAD EAGLES": "Go Ahead Eagles",
+    "GO AHEAD": "Go Ahead Eagles",
+    "ZWOLLE": "PEC Zwolle",
+    "PEC ZWOLLE": "PEC Zwolle",
+    "RODA JC": "Roda JC",
+    "RODA": "Roda JC",
+
+    // Uluslararası
+    "SHAKHTAR DONETSK": "Shakhtar Donetsk",
+    "SHAKHTAR": "Shakhtar Donetsk",
+    "DYNAMO KYIV": "Dynamo Kyiv",
+    "DINAMO ZAGREB": "Dinamo Zagreb",
+    "ZAGREB": "Dinamo Zagreb",
+    "STURM GRAZ": "Sturm Graz",
+    "STURM": "Sturm Graz",
+    "SALZBURG": "FC Salzburg",
+    "RED BULL SALZBURG": "FC Salzburg",
+    "RAPID WIEN": "Rapid Wien",
+    "WIEN": "Rapid Wien",
+    "AUSTRIA WIEN": "Austria Wien",
+    "AUSTRIA": "Austria Wien",
+    "BASEL": "FC Basel",
+    "FC BASEL": "FC Basel",
+    "YOUNG BOYS": "Young Boys",
+    "YB": "Young Boys",
+    "LECH POZNAŃ": "Lech Poznan",
+    "LECH POZNAN": "Lech Poznan",
+    "LEGIA WARSAW": "Legia Warszawa",
+    "LEGIA": "Legia Warszawa",
+    "BENFICA": "Benfica",
+    "PORTO": "Porto",
+    "FC PORTO": "Porto",
+    "SPORTING": "Sporting CP",
+    "SPORTING CP": "Sporting CP",
+    "CELTIC": "Celtic",
+    "CELTIC FC": "Celtic",
+    "RANGERS": "Rangers",
+    "FC RANGERS": "Rangers",
+    "PSFC CELTIC": "Celtic",
+    "BASEL": "FC Basel",
+    "ZURICH": "Zurich",
+    "FC ZURICH": "Zurich",
+    "LAUSANNE": "Lausanne-Sport",
+    "LAUSANNE-SPORT": "Lausanne-Sport",
+    "ST. GALLEN": "FC St. Gallen",
+    "GALATASARAY SK": "Galatasaray",
+    "FK CRVENA ZVEZZDA": "Crvena Zvezda",
+    "CRVENA ZVEZZDA": "Crvena Zvezda",
+    "PARTIZAN": "Partizan",
+    "FK PARTIZAN": "Partizan",
+    "OLYMPIACOS": "Olympiacos",
+    "AEK ATHENS": "AEK Athens",
+    "PANATHINAIKOS": "Panathinaikos",
+    "PANATHINAIKOS FC": "Panathinaikos",
+
+    // Arjantin
+    "VELEZ SARSFIELD": "Velez Sarsfield",
+    "VELEZ": "Velez Sarsfield",
+    "INDEPENDIENTE RIVADAVIA": "Independiente Rivadavia",
+    "INDEPENDIENTE": "Independiente",
+    "BOCA JUNIORS": "Boca Juniors",
+    "BOCA": "Boca Juniors",
+    "RIVER PLATE": "River Plate",
+    "RIVER": "River Plate",
+    "SAN LORENZO": "San Lorenzo",
+    "ESTUDIANTES": "Estudiantes",
+    "RACING": "Racing Club",
+    "RACING CLUB": "Racing Club",
+    "LANUS": "Lanus",
+    "LANÚS": "Lanus",
+    "DEFENSA Y JUSTICIA": "Defensa y Justicia",
+    "BANFIELD": "Banfield",
+
+    // Brezilya
+    "FLAMENGO": "Flamengo",
+    "PALMEIRAS": "Palmeiras",
+    "SANTOS": "Santos",
+    "CORINTHIANS": "Corinthians",
+    "CRUZEIRO": "Cruzeiro",
+    "ATLETICO MINEIRO": "Atletico Mineiro",
+    "ATLÉTICO MINEIRO": "Atletico Mineiro",
+    "GREMIO": "Gremio",
+    "GRÊMIO": "Gremio",
+    "INTERNACIONAL": "Internacional",
+    "BOTAFOGO": "Botafogo",
+    "VASCO DA GAMA": "Vasco da Gama",
+    "VASCO": "Vasco da Gama",
+    "VILA NOVA": "Vila Nova",
+    "VILA NOVA GO": "Vila Nova",
+    "CEBOLINHA": "Cebolinha",
+
+    // Diğer
+    "JEJU UNITED": "Jeju United",
+    "JEJU": "Jeju United",
+    "ULSAN HYUNDAI": "Ulsan Hyundai",
+    "ULSAN": "Ulsan Hyundai",
+    "KAWASAKI FRONTALE": "Kawasaki Frontale",
+    "AL RAYYAN": "Al Rayyan",
+    "RAYYAN": "Al Rayyan",
+    "AL AIN": "Al Ain",
+    "NEWCASTLE": "Newport County",
+    "NEWPORT": "Newport County",
+    "BRISTOL CITY": "Bristol City",
+    "COLCHESTER": "Colchester United",
+    "COLCHESTER UNITED": "Colchester United",
+    "WALSALL": "Walsall",
+    "PETERBOROUGH": "Peterborough United",
+    "PETERBOROUGH UNITED": "Peterborough United",
+    "BOLTON": "Bolton Wanderers",
+    "BOLTON WANDERERS": "Bolton Wanderers",
+    "COVENTRY": "Coventry City",
+    "COVENTRY CITY": "Coventry City",
+    "BLACKBURN": "Blackburn Rovers",
+    "BLACKBURN ROVERS": "Blackburn Rovers",
+    "WATFORD": "Watford",
+    "NORWICH": "Norwich City",
+    "NORWICH CITY": "Norwich City",
+    "MIDDLESBROUGH": "Middlesbrough",
+    "SUNDERLAND": "Sunderland",
+    "CARDIFF": "Cardiff City",
+    "CARDIFF CITY": "Cardiff City",
+    "MILLWALL": "Millwall",
+    "QUEENS PARK RANGERS": "Queens Park Rangers",
+    "QPR": "Queens Park Rangers",
+    "FULHAM": "Fulham",
+    "SWANSEA": "Swansea City",
+    "SWANSEA CITY": "Swansea City",
+    "STOKE CITY": "Stoke City",
+    "STOKE": "Stoke City",
+    "WEST BROMWICH": "West Bromwich Albion",
+    "WEST BROM": "West Bromwich Albion",
+    "DONCASTER": "Doncaster Rovers",
+    "DONCASTER ROVERS": "Doncaster Rovers",
+    "ROTHERHAM": "Rotherham United",
+    "ROTHERHAM UNITED": "Rotherham United",
+    "HULL CITY": "Hull City",
+    "HULL": "Hull City",
+    "BRENTFORD": "Brentford",
+    "BARNSLEY": "Barnsley",
+    "LINCOLN": "Lincoln City",
+    "LINCOLN CITY": "Lincoln City",
+    "SCUNTHORPE": "Scunthorpe United",
+    "SCUNTHORPE UNITED": "Scunthorpe United",
+    "GRIMSBY": "Grimsby Town",
+    "GRIMSBY TOWN": "Grimsby Town",
+    "CHELTENHAM": "Cheltenham Town",
+    "CHELTENHAM TOWN": "Cheltenham Town"
+};
+
+// Takım adını standarlaştır
+const standardizeTeamName = (name) => {
+    if (!name) return name;
+    const upper = name.toUpperCase().trim();
+    
+    // Direkt eşleşme
+    if (teamNameMapping[upper]) {
+        return teamNameMapping[upper];
+    }
+    
+    // Kısmi eşleşme (Contains)
+    for (const [sporekrani, sofascore] of Object.entries(teamNameMapping)) {
+        if (upper.includes(sporekrani) && sporekrani.length > 3) {
+            return sofascore;
+        }
+    }
+    
+    return name; // Eşleşme yoksa orijinalini döndür
+};
+
 async function getBroadcasterData() {
     const sports = ['futbol', 'basketbol', 'tenis'];
     const timeZone = 'Europe/Istanbul';
@@ -150,17 +531,22 @@ async function getBroadcasterData() {
                     rawChannels = m.lines.slice(1);
                 }
 
-             // 🌟 AKILLI BEYAZ LİSTE (WHITELIST) FİLTRESİ
-                // Kara liste yerine sadece bildiğimiz gerçek kanalları tutan aşılmaz duvar.
-                // yayinci.js içerisindeki validChannels güncellenmiş hali:
-const validChannels = [
-    "trt", "bein", "beın", "s sport", "ssport", "tivibu", "smart spor", "spor smart",
-    "d-smart", "euroleague tv", "nba tv", "nba league pass", "prime video", "amazon", "youtube", 
-    "exxen", "tv8", "a spor", "eurosport", "içtimai", "cbc sport", "idman", "az tv", 
-    "fb tv", "gs tv", "bjk tv", "kanal d", "star tv", "show tv", "atv", "ntv", "tabii", 
-    "red bull", "wta tv", "atp tv", "fiba tv", "tbf tv", "tv100", "yayın yok"
-];
+                // 🔥 YENİ: Maçtaki takım adlarını standarlaştır
+                const macParts = mac.split(/\s*[-–—]\s*|vs|VS/i);
+                if (macParts.length === 2) {
+                    const homeStandardized = standardizeTeamName(macParts[0].trim());
+                    const awayStandardized = standardizeTeamName(macParts[1].trim());
+                    mac = `${homeStandardized} - ${awayStandardized}`;
+                }
 
+                // 🌟 AKILLI BEYAZ LİSTE (WHITELIST) FİLTRESİ
+                const validChannels = [
+                    "trt", "bein", "beın", "s sport", "ssport", "tivibu", "smart spor", "spor smart",
+                    "d-smart", "euroleague tv", "nba tv", "nba league pass", "prime video", "amazon", "youtube", 
+                    "exxen", "tv8", "a spor", "eurosport", "içtimai", "cbc sport", "idman", "az tv", 
+                    "fb tv", "gs tv", "bjk tv", "kanal d", "star tv", "show tv", "atv", "ntv", "tabii", 
+                    "red bull", "wta tv", "atp tv", "fiba tv", "tbf tv", "tv100", "yayın yok", "caaf", "caaf tv"
+                ];
 
                 let filteredChannels = rawChannels.filter(line => {
                     if (!line || line.length < 2) return false;
@@ -171,21 +557,18 @@ const validChannels = [
                         return false;
                     }
 
-                    // Satırın içinde geçerli bir kanal adı var mı? (Örn: "Bein Sports Max 1" -> "bein" içeriyor, KABUL ET!)
-                    // İçermiyorsa (Örn: "Stuttgart Yarı", "İtalya Play-Off") -> REDDET!
+                    // Satırın içinde geçerli bir kanal adı var mı?
                     return validChannels.some(channel => l.includes(channel));
                 });
 
                 // Kalan tertemiz kanalları birleştir
                 let cleanYayin = filteredChannels.join(' / ')
                     .replace(/chevron_right/gi, '')
-                    .replace(/^[ \/]+|[ \/]+$/g, '')    // Baştaki/sondaki yalnız slash'leri sil
-                    .replace(/\s*\/\s*\/\s*/g, ' / ')   // Aralarda kalan / / gibi çoklu slashleri teke düşür
+                    .replace(/^[ \/]+|[ \/]+$/g, '')
+                    .replace(/\s*\/\s*\/\s*/g, ' / ')
                     .trim();
 
                 if (!cleanYayin || cleanYayin === '/' || cleanYayin.length < 2) cleanYayin = 'Yayın Yok';
-
-               
 
                 const lowerMac = mac.toLowerCase();
                 
@@ -199,30 +582,11 @@ const validChannels = [
                     mac.length < 5
                 ) return;
 
-                // 🔄 TAKIM İSMİ DÜZELTME SÖZLÜĞÜ (MAPPING)
-                const teamNameDictionary = {
-                    "BARCELONA": "Barça Basket",
-                    "IBEROSTAR TENERIFE": "La Laguna Tenerife"
-                    // İhtiyaç duydukça buraya "ESKİ İSİM": "YENİ İSİM" şeklinde ekleme yapabilirsin
-                };
-
-                // Önce ana maç metnini büyük harflere çevirip boşluklarını alıyoruz
-                let finalMac = mac.toUpperCase().trim();
-                
-                // Sözlükteki kelimeleri sırayla arayıp, bulursa Firebase'deki isimle değiştiriyoruz
-                Object.keys(teamNameDictionary).forEach(sporekraniName => {
-                    if (finalMac.includes(sporekraniName)) {
-                        finalMac = finalMac.replace(sporekraniName, teamNameDictionary[sporekraniName]);
-                    }
-                });
-
-                
-
                 if (allMatches[targetDate]) {
                     allMatches[targetDate].matches.push({
                         saat: m.saat,
                         spor: sportName,
-                        mac: finalMac,
+                        mac: mac,
                         yayin: cleanYayin
                     });
                 }
